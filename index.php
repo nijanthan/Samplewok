434687c004e30b97f4b7b39b13d27238008f02f9
jibi gopi <jibi@pluskb.com>
2013-03-14 23:48:14 +0530

    Home
    Home

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>index</title>
+</head>
+
+<body style="width:100%;height:600px;background:url(../../my%20work/Untitled-1.jpg)">
+
+<div style="margin:auto; width:200px;height:200px;">
+<?php 
+
+
+echo "<h1>PlUSKB INNOOVATIOM</h1> ";
+echo "<br /> Bangaklore";
+?>
+</div>
+</body>
+</html>
